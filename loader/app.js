19fa9fb6201fff0e78f83781789cd2ed66cb55f5
Henok Yoseph <henok.y2014@gmail.com>
2023-03-25 20:03:21 +0300

    Added backend url to cors

--- loader/app.js
+++ loader/app.js
@@ -16,12 +16,14 @@ const loginRouter = require('../api/login/router')
 //Create app
 const app = express();
 
+//Add cors for api use in frontend
+app.use(cors("https://alumni-track-system-kr9h.onrender.com"))
+
+
 //Add in the body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//Add cors for api use in frontend
-app.use(cors())
 
 //Use routers
 app.use('/api/v1/alumni',alumniRouter);
