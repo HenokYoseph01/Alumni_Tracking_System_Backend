ee2585b33c9ed2f7519e9573f2907d409fb3859f
Henok Yoseph <henok.y2014@gmail.com>
2023-03-26 21:31:57 +0300

    cors v4

--- loader/app.js
+++ loader/app.js
@@ -16,10 +16,19 @@ const loginRouter = require('../api/login/router')
 //Create app
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain)
+
 //Add cors for api use in frontend
 app.use(cors("https://alumni-track-system-kr9h.onrender.com"))
 
-app.set('Access-Control-Allow-Origin', '*')
+
 
 
 //Add in the body parser
