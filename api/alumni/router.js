164056ac59bf751abe072c36f8287d883eda9b95
Henok Yoseph <henok.y2014@gmail.com>
2023-03-28 21:17:58 +0300

    revert to original plus vercel

--- api/alumni/router.js
+++ api/alumni/router.js
@@ -1,57 +1,57 @@
-// //require express
-// const express = require('express');
+//require express
+const express = require('express');
 
-// //Router
-// const router = express.Router();
+//Router
+const router = express.Router();
 
-// //Protect
-// const protect = require('../protect')
+//Protect
+const protect = require('../protect')
 
-// //File uploader
-// const fileUploader = require('../../utils/fileUploader')
+//File uploader
+const fileUploader = require('../../utils/fileUploader')
 
-// //Allowed File type
-// const allowedFileType = require('../../utils/allowedFileType')
+//Allowed File type
+const allowedFileType = require('../../utils/allowedFileType')
 
-// //Controllers
-// const alumniController = require('./controller');
+//Controllers
+const alumniController = require('./controller');
 
-// // Alumni routes
-// router.post("/register",protect,allowedFileType('png','jpeg','jpg')
-//             ,fileUploader.single('avatar'),alumniController.register,alumniController.uploadPhoto);
+// Alumni routes
+router.post("/register",protect,allowedFileType('png','jpeg','jpg')
+            ,fileUploader.single('avatar'),alumniController.register,alumniController.uploadPhoto);
 
-// router.route('/')
-// .get(protect,alumniController.getAlumniProfile)
-// .patch(protect,alumniController.updateProfle)
+router.route('/')
+.get(protect,alumniController.getAlumniProfile)
+.patch(protect,alumniController.updateProfle)
 
-// router.patch('/changepassword',protect,alumniController.changePassword)
+router.patch('/changepassword',protect,alumniController.changePassword)
 
 
-// router.route('/forum')
-// .post(protect,alumniController.createForum)
-// .get(protect,alumniController.getAllForum)
+router.route('/forum')
+.post(protect,alumniController.createForum)
+.get(protect,alumniController.getAllForum)
 
-// router.get('/forum/me',protect,alumniController.getAuthorForum);
+router.get('/forum/me',protect,alumniController.getAuthorForum);
 
-// router.route('/forum/:forumId')
-// .get(protect,alumniController.getSingleForum)
-// .patch(protect,alumniController.updateForum)
-// .delete(protect,alumniController.deleteForum)
+router.route('/forum/:forumId')
+.get(protect,alumniController.getSingleForum)
+.patch(protect,alumniController.updateForum)
+.delete(protect,alumniController.deleteForum)
 
-// router.route('/forum/reply/:forumId')
-// .get(protect,alumniController.getReplyToForum)
-// .post(protect,alumniController.replyForum)
+router.route('/forum/reply/:forumId')
+.get(protect,alumniController.getReplyToForum)
+.post(protect,alumniController.replyForum)
 
-// router.route('/forum/reply/:forumId/:replyId')
-// .patch(protect,alumniController.updateRepliesToForum)
-// .delete(protect,alumniController.deleteRepliesToForum)
+router.route('/forum/reply/:forumId/:replyId')
+.patch(protect,alumniController.updateRepliesToForum)
+.delete(protect,alumniController.deleteRepliesToForum)
 
-// router.get('/forum/alumni/:forumId',protect,alumniController.getMinimumAlumniInfo)
+router.get('/forum/alumni/:forumId',protect,alumniController.getMinimumAlumniInfo)
 
-// router.post('/forum/report/:forumId',protect,alumniController.reportForum);
+router.post('/forum/report/:forumId',protect,alumniController.reportForum);
 
-// router.get('/event',protect,alumniController.getAllEvents)
-// router.get('/event/:eventId',protect,alumniController.getSingleEvent)
+router.get('/event',protect,alumniController.getAllEvents)
+router.get('/event/:eventId',protect,alumniController.getSingleEvent)
 
-// //Export router
-// module.exports = router
+//Export router
+module.exports = router
--- api/head/router.js
+++ api/head/router.js
@@ -1,36 +1,36 @@
-// //require express
-// const express = require('express');
+//require express
+const express = require('express');
 
-// //Router
-// const router = express.Router();
+//Router
+const router = express.Router();
 
-// //Protect
-// const protect = require('../protect')
+//Protect
+const protect = require('../protect')
 
-// //Controllers
-// const headController = require('./controller');
+//Controllers
+const headController = require('./controller');
 
-// //Routers
+//Routers
 
 
-// router.route('/')
-// .get(protect,headController.getHeadProfile)
-// .patch(protect,headController.updateHead)
+router.route('/')
+.get(protect,headController.getHeadProfile)
+.patch(protect,headController.updateHead)
 
-// router.route('/event')
-// .get(headController.getAllEvent)
-// .post(protect,headController.createEvent)
-// .delete(protect,headController.deleteAllEvent)
+router.route('/event')
+.get(headController.getAllEvent)
+.post(protect,headController.createEvent)
+.delete(protect,headController.deleteAllEvent)
 
-// router.route('/event/:eventId')
-// .get(headController.getSingleEvent)
-// .patch(protect,headController.updateEvent)
-// .delete(protect,headController.deleteSingleEvent)
+router.route('/event/:eventId')
+.get(headController.getSingleEvent)
+.patch(protect,headController.updateEvent)
+.delete(protect,headController.deleteSingleEvent)
 
-// router.get('/alumni',headController.getAlumnus)
-// router.post('/generatereport',protect,headController.generateReport)
+router.get('/alumni',headController.getAlumnus)
+router.post('/generatereport',protect,headController.generateReport)
 
-// router.get('/specific',protect,headController.findSpecficItem)
+router.get('/specific',protect,headController.findSpecficItem)
 
-// //Export router
-// module.exports = router
+//Export router
+module.exports = router
