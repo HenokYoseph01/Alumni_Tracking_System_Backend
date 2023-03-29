4d26d7998345596422de2de6ae1086c4a5eb219f
Henok Yoseph <henok.y2014@gmail.com>
2023-03-29 04:24:48 +0300

    work

--- loader/app.js
+++ loader/app.js
@@ -18,7 +18,15 @@ const app = express();
 
 
 //Add cors for api use in frontend
-app.options('*', cors())
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+  })
 
 
 //Add in the body parser
