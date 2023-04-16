3782468094c68e87fe1da0f318100d53a8a8faf3
Henok Yoseph <henok.y2014@gmail.com>
2023-04-16 07:43:59 +0300

    CORS update 2

--- api/geh/index.js
+++ api/geh/index.js
@@ -3,6 +3,10 @@ const config = require("../../config");
 
 // Send Development error
 const sendDevError = (err, res) => {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
@@ -16,15 +20,6 @@ const geh = (err, req, res, next) => {
   // Get the status and status code
   err.status = err.status || "ERROR";
   err.statusCode = err.statusCode || 500;
-
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  //intercept the OPTIONS call so we don't double up on calls to the integration
-  if ('OPTIONS' === res.method) {
-    res.send(200);
-  } else {
-    next();
-  }
 
   // Handle error for development environment
   if (config.env === "development") {
